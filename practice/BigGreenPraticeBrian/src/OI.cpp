ba3d9f0950dc73ede3e506aa897ab533418b4a07
EDSS Programmer <frc4917@gmail.com>
2016-12-02 11:49:18 -0500

    Brian project update

--- practice/BigGreenPraticeBrian/src/OI.cpp
+++ practice/BigGreenPraticeBrian/src/OI.cpp
@@ -1,7 +1,8 @@
 #include "OI.h"
-
+#include "Commands/DriveForwardCmd.h"
 OI::OI()
-{
+
 	driverController.reset(new Joystick(DRIVER_CONTROLLER_PORT));
+	drivetrainBtn.reset
 	// Process operator interface input here.
 }
