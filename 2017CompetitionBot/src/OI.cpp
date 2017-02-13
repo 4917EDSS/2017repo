87362c4171883e5768251d11f61e605003ccc771
4917 Programmer Driver Station <4917programmer@gmail.com>
2017-02-13 15:53:54 -0500

    Added Operator controller

--- 2017CompetitionBot/src/OI.cpp
+++ 2017CompetitionBot/src/OI.cpp
@@ -12,26 +12,27 @@
 OI::OI() {
 	// Process operator interface input here.
 	driverController.reset(new frc::Joystick(DRIVER_CONTROLLER_PORT));
+	operatorController.reset(new frc::Joystick(OPERATOR_CONTROLLER_PORT));
 
-	dIntakeBtn.reset(new frc::JoystickButton(driverController.get(), DRIVER_INTAKE_BTN));
+	dIntakeBtn.reset(new frc::JoystickButton(operatorController.get(), DRIVER_INTAKE_BTN));
 	dIntakeBtn->WhileHeld(new RunPickupCmd());
-	liftBtn.reset(new JoystickButton(driverController.get(), DRIVER_LIFT_BTN));
+	liftBtn.reset(new JoystickButton(operatorController.get(), DRIVER_LIFT_BTN));
 	liftBtn->WhileHeld(new LiftCmd(1.0));
-	lowerBtn.reset(new JoystickButton(driverController.get(), DRIVER_LOWER_BTN));
+	lowerBtn.reset(new JoystickButton(operatorController.get(), DRIVER_LOWER_BTN));
 	lowerBtn->WhileHeld(new LiftCmd(-1.0));
-	shooterEnableBtn.reset(new JoystickButton(driverController.get(), DRIVER_SHOOT_BTN));
+	shooterEnableBtn.reset(new JoystickButton(operatorController.get(), DRIVER_SHOOT_BTN));
 	shooterEnableBtn->WhileHeld(new ShootCmd());
-	increaseSpeedBtn.reset(new JoystickButton(driverController.get(), DRIVER_INCREASE_SPEED_BTN));
+	increaseSpeedBtn.reset(new JoystickButton(operatorController.get(), DRIVER_INCREASE_SPEED_BTN));
 	increaseSpeedBtn->WhenPressed(new IncreaseSpeedCmd());
-	decreaseSpeedBtn.reset(new JoystickButton(driverController.get(), DRIVER_DECREASE_SPEED_BTN));
+	decreaseSpeedBtn.reset(new JoystickButton(operatorController.get(), DRIVER_DECREASE_SPEED_BTN));
 	decreaseSpeedBtn->WhenPressed(new DecreaseSpeedCmd());
-	driveTurnBtn.reset(new JoystickButton(driverController.get(), DRIVER_DRIVETURN_BTN));
-	driveTurnBtn->WhenPressed(new DriveTurnCmd(60));
 	shiftBtn.reset(new JoystickButton(driverController.get(), DRIVER_SHIFT_BTN));
 	shiftBtn->WhenPressed(new ToggleShifterCmd());
 
 }
 std::shared_ptr<frc::Joystick> OI::getDriverController()
+std::shared_ptr<frc::Joystick> OI::getOperatorController()
 {
    return driverController;
+   return operatorController;
 }
--- 2017CompetitionBot/src/OI.h
+++ 2017CompetitionBot/src/OI.h
@@ -26,24 +26,24 @@
 
 // Joystick Ports
 const int DRIVER_CONTROLLER_PORT = 0;
-
+const int OPERATOR_CONTROLLER_PORT = 1;
 // Joystick Axis
 const int DRIVER_LEFT_TANK_DRIVE_AXIS = 1;
 const int DRIVER_RIGHT_TANK_DRIVE_AXIS = 3;
 
 // Joystick Buttons
-const int DRIVER_LIFT_BTN = 4;
-const int DRIVER_LOWER_BTN = 3;
-const int DRIVER_INTAKE_BTN = 1;
-const int DRIVER_SHOOT_BTN = 2;
-const int DRIVER_INCREASE_SPEED_BTN = 6;
-const int DRIVER_DECREASE_SPEED_BTN = 5;
-const int DRIVER_DRIVETURN_BTN =7;
+const int OPERATOR_LIFT_BTN = 4;
+const int OPERATOR_LOWER_BTN = 3;
+const int OPERATOR_INTAKE_BTN = 1;
+const int OPERATOR_SHOOT_BTN = 2;
+const int OPERATOR_INCREASE_SPEED_BTN = 6;
+const int OPERATOR_DECREASE_SPEED_BTN = 5;
 const int DRIVER_SHIFT_BTN =8;
 
 class OI {
 private:
 	std::shared_ptr<frc::Joystick> driverController;
+	std::shared_ptr<frc::Joystick> operatorController;
 	std::shared_ptr<frc::JoystickButton>liftBtn;
 	std::shared_ptr<frc::JoystickButton>lowerBtn;
 	std::shared_ptr<frc::JoystickButton> dIntakeBtn;
@@ -56,6 +56,7 @@ private:
 public:
 	OI();
 	std::shared_ptr<frc::Joystick> getDriverController();
+	std::shared_ptr<frc::Joystick> getOperatorController();
 };
 
 #endif  // OI_H
