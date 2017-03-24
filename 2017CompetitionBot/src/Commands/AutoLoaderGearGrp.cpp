fc83b9f5f5dd34764bb2b89076c9f5b7082ff8f6
EDSS Programmer <frc4917@gmail.com>
2017-03-24 12:46:07 -0400

    Best guess at fixing loader gear auto

--- 2017CompetitionBot/src/Commands/AutoLoaderGearGrp.cpp
+++ 2017CompetitionBot/src/Commands/AutoLoaderGearGrp.cpp
@@ -19,6 +19,7 @@ AutoLoaderGearGrp::AutoLoaderGearGrp() {
 	//Keep Driving Straight
 	//AddSequential(new SilkyDriveCmd(std::vector<double> {0, 500, 1300}, std::vector<double> {0, 500, 1300}));
 
+	AddSequential(new SilkyDriveCmd(std::vector<double> {0, 1300, 1600, 3200}, std::vector<double> {0, 1300, 2400, 3850}));
 	//Wait
 	AddSequential(new WaitCommand(GEAR_WAIT_TIME));
 	AddSequential(new SilkyDriveCmd(std::vector<double> {0, -1000, -1500}, std::vector<double> {0, -1000, -2420}));
