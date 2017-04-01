86590759cb69f0a6917df868d7922ce8960a50f3
EDSS Programmer <frc4917@gmail.com>
2017-04-01 16:22:11 -0400

    Boiler Gear

--- 2017CompetitionBot/src/Commands/AutoBoilerGearShootGrp.cpp
+++ 2017CompetitionBot/src/Commands/AutoBoilerGearShootGrp.cpp
@@ -14,7 +14,7 @@ AutoBoilerGearShootGrp::AutoBoilerGearShootGrp() {
 	AddParallel(new OpenGearFlapsCmd());
 
 	//Drive to boiler gear
-	AddSequential(new SilkyDriveCmd(std::vector<double> {0, 775 - 150, 2435 -150}, std::vector<double> {0, 775 - 150, 1625 - 150},
+	AddSequential(new SilkyDriveCmd(std::vector<double> {0, 775 - 212, 2450 -212}, std::vector<double> {0, 775 - 212, 1625 - 212},
 									std::vector<double> {0, 930 + 44, 1477 + 44}, std::vector<double> {0, 930 + 44, 2233 + 44}));
 	AddSequential(new SilkyDriveCmd(std::vector<double> {0, 450, 1400}, std::vector<double> {0, 450, 1400},
 									std::vector<double> {0, 600, 1475}, std::vector<double> {0, 600, 1475}));
@@ -28,7 +28,7 @@ AutoBoilerGearShootGrp::AutoBoilerGearShootGrp() {
 	AddSequential(new SilkyDriveCmd(std::vector<double> {0, -200, -300}, std::vector<double> {0, -200, -2300},
 									std::vector<double> {0, -350, -2550}, std::vector<double> {0, -350, -550}));
 	//Drive to boiler
-	AddSequential(new SilkyDriveCmd(std::vector<double> {0, 1250, 2550, 2555}, std::vector<double> {0, 1250, 2050, 2225},
+	AddSequential(new SilkyDriveCmd(std::vector<double> {0, 400, 1900, 2325}, std::vector<double> {0, 400, 1500, 1843},
 	/* John Example For Future Use:
 	int delta1L = 0;
 	int delta2L = 0;
