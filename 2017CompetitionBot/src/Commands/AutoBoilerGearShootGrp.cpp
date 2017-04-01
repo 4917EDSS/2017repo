d28b171c8fb816561d54f71df67f9c867e08e1c8
EDSS Programmer <frc4917@gmail.com>
2017-04-01 14:51:38 -0400

    Boiler Gear

--- 2017CompetitionBot/src/Commands/AutoBoilerGearShootGrp.cpp
+++ 2017CompetitionBot/src/Commands/AutoBoilerGearShootGrp.cpp
@@ -14,9 +14,9 @@ AutoBoilerGearShootGrp::AutoBoilerGearShootGrp() {
 	AddParallel(new OpenGearFlapsCmd());
 
 	//Drive to boiler gear
-	AddSequential(new SilkyDriveCmd(std::vector<double> {0, 875, 2535}, std::vector<double> {0, 875, 1725},
-									std::vector<double> {0, 930, 1477}, std::vector<double> {0, 930, 2233}));
-	AddSequential(new SilkyDriveCmd(std::vector<double> {0, 450, 1300}, std::vector<double> {0, 450, 1300},
+	AddSequential(new SilkyDriveCmd(std::vector<double> {0, 775 - 150, 2435 -150}, std::vector<double> {0, 775 - 150, 1625 - 150},
+									std::vector<double> {0, 930 + 44, 1477 + 44}, std::vector<double> {0, 930 + 44, 2233 + 44}));
+	AddSequential(new SilkyDriveCmd(std::vector<double> {0, 450, 1400}, std::vector<double> {0, 450, 1400},
 									std::vector<double> {0, 600, 1475}, std::vector<double> {0, 600, 1475}));
 
 	//Wait at gear
@@ -28,8 +28,14 @@ AutoBoilerGearShootGrp::AutoBoilerGearShootGrp() {
 	AddSequential(new SilkyDriveCmd(std::vector<double> {0, -200, -300}, std::vector<double> {0, -200, -2300},
 									std::vector<double> {0, -350, -2550}, std::vector<double> {0, -350, -550}));
 	//Drive to boiler
-	AddSequential(new SilkyDriveCmd(std::vector<double> {0, 1000, 2300, 2800}, std::vector<double> {0, 1000, 1800, 2200},
-									//std::vector<double> {0, 1000, 1800, 2200}, std::vector<double> {0, 1000, 2300, 2800}));
+	AddSequential(new SilkyDriveCmd(std::vector<double> {0, 1000 + 250, 2300 + 250, 2400 + 150}, std::vector<double> {0, 1000 + 250, 1800 + 250, 2100 + 125},
+	/* John Example For Future Use:
+	int delta1L = 0;
+	int delta2L = 0;
+	int delta3L = 0;
+	AddSequential(new SilkyDriveCmd(std::vector<double> {0, delta1, delta1 + delta2, delta1 + delta2 + delta3}, std::vector<double> {}),
+	 	 	 	 	 	 	 	 	 std::vector<double> {}, std::vector<double> {}
+	 */
 									std::vector<double> {0, 400, 1400, 1643}, std::vector<double> {0, 400, 1900, 2225}));
 
 	//Pulse hopper
