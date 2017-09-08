430b9d0532479819940020fffb183432b6bbda17
Eric Gaudet <ericjpgaudet@yahoo.ca>
2017-09-08 14:26:49 -0400

    Added test app for syslog logging functionality

---
+++ practice/Logging/src/Logging.cpp
@@ -0,0 +1,10 @@
+/*
+ * Logging.cpp
+ *
+ *  Created on: Sep 8, 2017
+ *      Author: Gaudets
+ */
+
+
+
+
---
+++ practice/Logging/src/Logging.h
@@ -0,0 +1,15 @@
+/*
+ * Logging.h
+ *
+ *  Created on: Sep 8, 2017
+ *      Author: Gaudets
+ */
+
+#ifndef SRC_LOGGING_H_
+#define SRC_LOGGING_H_
+
+
+
+
+
+#endif /* SRC_LOGGING_H_ */
